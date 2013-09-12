b412a4f72d79f6ddcaa3655fb6f3e7f259e23f60
Seth Hallem <seth.hallem@stanfordalumni.org>
2013-09-12 16:50:11 -0700

    Updated web services projects

--- src/com/mobilehelix/services/objects/ApplicationServerCreateSessionRequest.java
+++ src/com/mobilehelix/services/objects/ApplicationServerCreateSessionRequest.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 Mobile Helix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.mobilehelix.services.objects;
 
@@ -19,7 +30,7 @@ import org.codehaus.jackson.JsonToken;
  *
  * @author shallem
  */
-public class CreateSessionRequest {
+public class ApplicationServerCreateSessionRequest {
     private String deviceRegion;
     private Double deviceLatitude;
     private Double deviceLongitude;
@@ -34,7 +45,7 @@ public class CreateSessionRequest {
     private Integer[] appGenIDs;
     private byte[] serverSessionID;
     
-    public CreateSessionRequest() {
+    public ApplicationServerCreateSessionRequest() {
     }
     
     public int getSessionExpirationType() {
@@ -188,9 +199,9 @@ public class CreateSessionRequest {
         return baos.toByteArray();
     }
     
-    public static CreateSessionRequest fromBson(byte[] data) throws IOException,
+    public static ApplicationServerCreateSessionRequest fromBson(byte[] data) throws IOException,
             ParseException {
-        CreateSessionRequest ret = new CreateSessionRequest();
+        ApplicationServerCreateSessionRequest ret = new ApplicationServerCreateSessionRequest();
         List<Long> appIDs = new LinkedList<>();
         List<Integer> appGenIDs = new LinkedList<>();
         
