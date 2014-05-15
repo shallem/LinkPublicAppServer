5eaea23a0693c1b4f2090590372ab2d0dfb8d8a6
Seth Hallem <seth@mobilehelix.com>
2014-05-15 10:13:29 -0400

    Updated server type contants to contain frederic's new type.

--- WebServicesObjects/src/com/mobilehelix/services/constants/ServerTypeConstants.java
+++ WebServicesObjects/src/com/mobilehelix/services/constants/ServerTypeConstants.java
@@ -19,6 +19,6 @@ public class ServerTypeConstants {
     public static final int SERVER_TYPE_POSTGRES_SERVER = 4;
     public static final int SERVER_TYPE_PUSH_SERVER = 5;
     public static final int SERVER_TYPE_ROUTER_SERVER = 6;
-    public static final int SERVER_TYPE_JIRA_SERVER = 7;
+    public static final int SERVER_TYPE_APPLICATION_GATEWAY = 7;
     public static final int SERVER_TYPE_CUSTOM = 255;
 }
