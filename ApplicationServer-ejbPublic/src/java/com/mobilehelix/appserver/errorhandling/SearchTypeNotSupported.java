dc7ffe2a8bd691e6bd568fb760ac8de533090fd1
Frederic Langlet <flanglet@gmail.com>
2015-04-08 15:20:31 -0700

    Add max file download sixe to session.

--- ApplicationServer-ejbPublic/src/java/com/mobilehelix/appserver/errorhandling/SearchTypeNotSupported.java
+++ ApplicationServer-ejbPublic/src/java/com/mobilehelix/appserver/errorhandling/SearchTypeNotSupported.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mobilehelix.appserver.errorhandling;
 
 /**
- *
+ * Extends AppserverSystemException to convey the information that the
+ * underlying file system does not support the type of search provided in the
+ * request. The goal is to make it easy to manage this kind of error separately from
+ * actual query failures
  * @author Seth
  */
 public class SearchTypeNotSupported extends AppserverSystemException {
